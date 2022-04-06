de3b44b57c7a60dd6cb2a76f7c1abc0648004ec1
manulon <mlongoe@fi.uba.ar>
2022-04-06 10:12:51 -0300

    codigo final

--- TP1/codigo.py
+++ TP1/codigo.py
@@ -1,18 +1,3 @@
-'''
-    Sea P un set de datos
-	Sea A el conjunto final
-	
-	Ordenar P por lugar de inicio de cobertura
-
-	Mientras P no este vacío
-		Sea i el pedido en P con menor lugar de inicio de cobertura
-		Agrego i a la lista A
-		Quito de P todas las antenas solapadas con i
-		Si el primer elemento de P tiene un lugar de inicio mayor al ultimo punto cubierto por el ultimo 
-        elemento de la lista A
-			retornar que no se puede cubrir toda la ruta
-	retornar que se puede conectar toda la ruta y las antenas que deben conectarse
-'''
 import sys
 
 def criterioOrdenamiento(setDeDatos):
